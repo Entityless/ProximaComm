51b41403d53b829792db5f16f24ac5a546a106aa
Entityless <entityless@gmail.com>
2019-06-09 01:13:34 +0800

    better profile code

--- main.cpp
+++ main.cpp
@@ -13,16 +13,17 @@ using namespace std;
 
 // loop_cnt arr_sz comp_nthreads
 int main(int argc, char** argv) {
-    if (argc != 5) {
-        printf("params: <loop count> <arr size> <sum nthreads> <slice>\n");
+    if (argc != 6) {
+        printf("params: <profile count> <loop count> <arr size> <sum nthreads> <slice>\n");
         exit(0);
     }
 
-    int loop_cnt, arr_sz, comp_nthreads, slice;
-    loop_cnt = atoi(argv[1]);
-    arr_sz = atoi(argv[2]);
-    comp_nthreads = atoi(argv[3]);
-    slice = atoi(argv[4]);
+    int profile_cnt, loop_cnt, arr_sz, comp_nthreads, slice;
+    profile_cnt = atoi(argv[1]);
+    loop_cnt = atoi(argv[2]);
+    arr_sz = atoi(argv[3]);
+    comp_nthreads = atoi(argv[4]);
+    slice = atoi(argv[5]);
 
     MPI_Init(nullptr, nullptr);
 
@@ -39,57 +40,59 @@ int main(int argc, char** argv) {
     double* send_arr = (double*) _mm_malloc(sizeof(double) * arr_sz, 4096);
     double* recv_arr = (double*) _mm_malloc(sizeof(double) * arr_sz, 4096);
     double sum;
-
-    for (int i = 0; i < arr_sz; i++) {
-        if (my_rank == 0)
-            send_arr[i] = comm_sz - 1.000001;
-        else
-            send_arr[i] = -1.0;
-    }
-
     double stt, edt;
 
-    stt = MPI_Wtime();
-    for (int i = 0; i < loop_cnt; i++) {
-        MPI_Allreduce(MPI_IN_PLACE, send_arr, arr_sz, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    for (int l = 0; l < profile_cnt; l++) {
+        for (int i = 0; i < arr_sz; i++) {
+            if (my_rank == 0)
+                send_arr[i] = comm_sz - 1.000000000001;
+            else
+                send_arr[i] = -1.0;
+        }
+
+
+        stt = MPI_Wtime();
+        for (int i = 0; i < loop_cnt; i++) {
+            MPI_Allreduce(MPI_IN_PLACE, send_arr, arr_sz, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        }
+        edt = MPI_Wtime();
+
+        sum = 0;
+        for (int i = 0; i < arr_sz; i++)
+            sum += send_arr[i];
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        printf("normal allreduce finished, checksum = %f, duration = %f\n", sum, edt - stt);
+        fflush(stdout);
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        for (int i = 0; i < arr_sz; i++) {
+            if (my_rank == 0)
+                send_arr[i] = comm_sz - 1.000000000001;
+            else
+                send_arr[i] = -1.0;
+        }
+
+        Proxima<ProximaWeirdLocker> p;
+        p.ForkThreadsForAllreduce(comp_nthreads, slice);
+
+        stt = MPI_Wtime();
+        for (int i = 0; i < loop_cnt; i++) {
+            p.AllreduceSum(send_arr, recv_arr, arr_sz, MPI_COMM_WORLD);
+        }
+        edt = MPI_Wtime();
+
+        sum = 0;
+        for (int i = 0; i < arr_sz; i++)
+            sum += send_arr[i];
+
+        p.FinalizeThreads();
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        printf("weird allreduce finished, checksum = %f, duration = %f\n", sum, edt - stt);
+        fflush(stdout);
+        MPI_Barrier(MPI_COMM_WORLD);
     }
-    edt = MPI_Wtime();
-
-    sum = 0;
-    for (int i = 0; i < arr_sz; i++)
-        sum += send_arr[i];
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    printf("normal allreduce finished, checksum = %f, duration = %f\n", sum, edt - stt);
-    fflush(stdout);
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    for (int i = 0; i < arr_sz; i++) {
-        if (my_rank == 0)
-            send_arr[i] = comm_sz - 1.000001;
-        else
-            send_arr[i] = -1.0;
-    }
-
-    Proxima<ProximaWeirdLocker> p;
-    p.ForkThreadsForAllreduce(comp_nthreads, slice);
-
-    stt = MPI_Wtime();
-    for (int i = 0; i < loop_cnt; i++) {
-        p.AllreduceSum(send_arr, recv_arr, arr_sz, MPI_COMM_WORLD);
-    }
-    edt = MPI_Wtime();
-
-    sum = 0;
-    for (int i = 0; i < arr_sz; i++)
-        sum += send_arr[i];
-
-    p.FinalizeThreads();
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    printf("weird allreduce finished, checksum = %f, duration = %f\n", sum, edt - stt);
-    fflush(stdout);
-    MPI_Barrier(MPI_COMM_WORLD);
 
     // p.Allreduce();
 
